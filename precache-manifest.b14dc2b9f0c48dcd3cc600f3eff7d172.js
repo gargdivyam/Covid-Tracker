5f56ea5e0b008e62d6230a1c6492c3b8f2431f90
gargdivyam327 <gargdivyam327@gmail.com>
2020-04-19 12:53:32 +0530

    Updates

---
+++ precache-manifest.b14dc2b9f0c48dcd3cc600f3eff7d172.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "109021d59cc5fd59847b62017e70b902",
+    "url": "/Covid-Tracker/index.html"
+  },
+  {
+    "revision": "5ffc8f26ab2b91177c3f",
+    "url": "/Covid-Tracker/static/css/main.0758ed1e.chunk.css"
+  },
+  {
+    "revision": "0e498502a3d38dfe8245",
+    "url": "/Covid-Tracker/static/js/2.a4911693.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Covid-Tracker/static/js/2.a4911693.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5ffc8f26ab2b91177c3f",
+    "url": "/Covid-Tracker/static/js/main.13a65253.chunk.js"
+  },
+  {
+    "revision": "97aa922749401d29aa9b",
+    "url": "/Covid-Tracker/static/js/runtime-main.183cb7d1.js"
+  }
+]);
